549ec5e86ba6b2241532e9d6103efce937ac77ab
pareramelisa <pareralucenamelisa@gmail.com>
2023-06-29 23:37:11 -0300

    rym

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import style from "./card.module.css"
 export default function Card(props) {
    return (
       <div className={style.cardContainer}>
-         <button className={style.boton} onClick={props.onClose}>X</button>
+         <button className={style.btn} onClick={props.onClose}></button>
          <img className={style.image} src={props.image} alt='not found' />
          <h2 className={style.name}>{props.name}</h2>
          <h2 className={style.item}>{props.status}</h2>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,9 @@
 import style from "./searchBar.module.css"
 export default function SearchBar(props) {
    return (
-      <div className={style.container}>
-         <input type='search' />
-         <button onClick={props.onSearch}>Agregar</button>
+      <div className={style.containerSearchBar}>
+         <input className={style.input} type='search' />
+         <button className={style.button} onClick={props.onSearch}>Search</button>
       </div>
    );
 }
